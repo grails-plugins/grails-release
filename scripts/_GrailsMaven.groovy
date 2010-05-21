815d027c056c8d7f8c1a0d549806d87a1a2468cf
Peter Ledbrook <peter@cacoethes.co.uk>
2010-05-21 16:42:40 +0100

    The plugin now respects the "groupId" property on plugin descriptors. It also preferentially reads the grails.project.groupId setting from BuildConfig.groovy rather than Config.groovy.

--- scripts/_GrailsMaven.groovy
+++ scripts/_GrailsMaven.groovy
@@ -23,95 +23,101 @@ includeTargets << grailsScript("_GrailsPackage")
 
 artifact = groovy.xml.NamespaceBuilder.newInstance(ant, 'antlib:org.apache.maven.artifact.ant')
 
-packageApp()
+target(init: "Initialisation for maven deploy/install") {
+	depends(packageApp)
 
-plugin = pluginManager?.allPlugins?.find { it.basePlugin }
-pomFileLocation = "${grailsSettings.projectTargetDir}/pom.xml"
-basePom = new File( "${basedir}/pom.xml" )
-if(basePom.exists())
-	pomFileLocation = basePom.absolutePath
-
-
-
-if(!plugin) {
-	includeTargets << grailsScript("_GrailsWar")	
-	war()
-}
-else {
-	includeTargets << grailsScript("_GrailsPluginDev")	
-	packagePlugin()
 	plugin = pluginManager?.allPlugins?.find { it.basePlugin }
-	pluginInstance = plugin.pluginClass.newInstance()	
-}
+	pomFileLocation = "${grailsSettings.projectTargetDir}/pom.xml"
+	basePom = new File( "${basedir}/pom.xml" )
+	if(basePom.exists())
+		pomFileLocation = basePom.absolutePath
 
-if(!basePom.exists()) {
-	new File(pomFileLocation).withWriter { w ->
-		xml = new groovy.xml.MarkupBuilder(w)
 
-		xml.project {
-			modelVersion "4.0.0"
-			if(plugin) {
-				if(pluginInstance.hasProperty('group'))
-					groupId pluginInstance.group ?: "org.grails.plugins"
-				else
-					groupId "org.grails.plugins"				
+
+	if(!plugin) {
+		includeTargets << grailsScript("_GrailsWar")	
+		war()
+	}
+	else {
+		includeTargets << grailsScript("_GrailsPluginDev")	
+		packagePlugin()
+		plugin = pluginManager?.allPlugins?.find { it.basePlugin }
+		pluginInstance = plugin.pluginClass.newInstance()	
+	}
+
+	if(!basePom.exists()) {
+		new File(pomFileLocation).withWriter { w ->
+			xml = new groovy.xml.MarkupBuilder(w)
+
+			xml.project {
+				modelVersion "4.0.0"
+				if(plugin) {
+					def group = "org.grails.plugins"
+					if(pluginInstance.hasProperty('group') && pluginInstance.group) {
+						group = pluginInstance.group
+					}
+					else if(pluginInstance.hasProperty('groupId') && pluginInstance.groupId) {
+						group = pluginInstance.groupId
+					}
+
+					groupId group
+					artifactId plugin.fileSystemShortName 
+					packaging "zip"
+					version plugin.version
+					name plugin.fileSystemShortName					
+				}
+				else {
+					groupId buildConfig.grails.project.groupId ?: (config?.grails?.project?.groupId ?: grailsAppName)
+					artifactId grailsAppName
+					packaging "war"
+					version grailsAppVersion
+					name grailsAppName				
+				}
 					
-				artifactId plugin.fileSystemShortName 
-				packaging "zip"
-				version plugin.version
-				name plugin.fileSystemShortName					
-			}
-			else {
-				groupId config?.grails?.project?.groupId ?: grailsAppName
-				artifactId GrailsNameUtils.getScriptName(grailsAppName)
-				packaging "war"
-				version grailsAppVersion
-				name grailsAppName				
-			}
-				
-				
-			if(plugin && plugin.dependencyNames) {
-				dependencies {					
-					corePlugins = pluginManager.allPlugins.findAll { it.pluginClass.name.startsWith("org.codehaus.groovy.grails.plugins") }*.name	
 					
-	 				for(dep in pluginInstance.dependsOn) {
-						String depName = dep.key
-						if(!corePlugins.contains(dep.key)) {
-							// Note: specifying group in dependsOn is a Grails 1.3 feature
-							// 1.2 users don't have this capability
-							def depGroup = "org.grails.plugins"
-							if(depName.contains(":")) {
-								def i = depName.split(":")
-								depGroup = i[0]
-								depName = i[1]
-							}
-							String depVersion = dep.value
-							def upper = GrailsPluginUtils.getUpperVersion(depVersion)
-							def lower = GrailsPluginUtils.getLowerVersion(depVersion)
-							if(upper == lower) depVersion = upper
-							else {
-								upper = upper == '*' ? '' : upper
-								lower = lower == '*' ? '' : lower
+				if(plugin && plugin.dependencyNames) {
+					dependencies {					
+						corePlugins = pluginManager.allPlugins.findAll { it.pluginClass.name.startsWith("org.codehaus.groovy.grails.plugins") }*.name	
+						
+						for(dep in pluginInstance.dependsOn) {
+							String depName = dep.key
+							if(!corePlugins.contains(dep.key)) {
+								// Note: specifying group in dependsOn is a Grails 1.3 feature
+								// 1.2 users don't have this capability
+								def depGroup = "org.grails.plugins"
+								if(depName.contains(":")) {
+									def i = depName.split(":")
+									depGroup = i[0]
+									depName = i[1]
+								}
+								String depVersion = dep.value
+								def upper = GrailsPluginUtils.getUpperVersion(depVersion)
+								def lower = GrailsPluginUtils.getLowerVersion(depVersion)
+								if(upper == lower) depVersion = upper
+								else {
+									upper = upper == '*' ? '' : upper
+									lower = lower == '*' ? '' : lower
+									
+									depVersion = "[$upper,$lower]"
+								}
 								
-								depVersion = "[$upper,$lower]"
+								dependency {
+									groupId depGroup
+									artifactId depName
+									version depVersion
+								}							
 							}
-							
-							dependency {
-								groupId depGroup
-								artifactId depName
-								version depVersion
-							}							
-						}
-					}					
+						}					
+					}
 				}
 			}
 		}
-	}
-	
+	}	
 }
 
 
 target(mavenInstall:"Installs a plugin or application into your local Maven cache") {
+	depends(init)
 	def deployFile = plugin ? new File(pluginZip) : grailsSettings.projectWarFile
 	def ext = plugin ? "zip" : "war"
 	installOrDeploy(deployFile, ext, false)
@@ -123,8 +129,9 @@ private generateChecksum(File file) {
 	return checksum
 }
 private installOrDeploy(File file, ext, boolean deploy, repos = [:]) {
-	if(deploy) {
-		parseArguments()
+	if (!deploy) {
+	        ant.checksum file:pomFileLocation, algorithm:"sha1", todir:projectTargetDir
+	        ant.checksum file:file, algorithm:"sha1", todir:projectTargetDir		
 	}
 
 	def pomCheck = generateChecksum(new File(pomFileLocation))
@@ -134,6 +141,12 @@ private installOrDeploy(File file, ext, boolean deploy, repos = [:]) {
 		if(ext == 'zip') {
 			attach file:"${basedir}/plugin.xml",type:"xml", classifier:"plugin"
 		}			
+
+        if (!deploy) {
+            attach file:"${projectTargetDir}/pom.xml.sha1",type:"pom.sha1"
+            attach file:"${projectTargetDir}/${file.name}.sha1",type:"${ext}.sha1"
+        }
+
         pom(file: pomFileLocation)
 		if(repos.remote) {
 			def repo = repos.remote
@@ -152,8 +165,7 @@ private installOrDeploy(File file, ext, boolean deploy, repos = [:]) {
 }
 
 target(mavenDeploy:"Deploys the plugin to a Maven repository") {
-	depends(parseArguments)
-		
+	depends(init)
 	def protocols = [ 	http: "wagon-http",
 						scp:	"wagon-ssh",
 						scpexe:	"wagon-ssh-external",
