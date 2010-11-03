6d600fd5a95196447b5f5303e147167d9d3a3f76
Peter Ledbrook <peter@cacoethes.co.uk>
2010-11-03 05:16:39 +0000

    Bumped version to 0.7.1 and added an exclusion on the 'groovy' dependency of http-builder. This is because without it, Grails takes 20s or more to resolve the dependencies! Anyway, groovy is not required because Grails provides groovy-all.

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -3,30 +3,22 @@ grails.project.test.class.dir = "target/test-classes"
 grails.project.test.reports.dir	= "target/test-reports"
 grails.project.dependency.resolution = {
     // inherit Grails' default dependencies
-    inherits( "global" ) {
-        // uncomment to disable ehcache
-        // excludes 'ehcache'
-    }
-    log "warn" // log level of Ivy resolver, either 'error', 'warn', 'info', 'debug' or 'verbose'
+    inherits "global"
+    log "warn"
+
     repositories {        
         grailsPlugins()
         grailsHome()
 
-        // uncomment the below to enable remote dependency resolution
-        // from public Maven repositories
         mavenLocal()
         mavenCentral()
-        //mavenRepo "http://snapshots.repository.codehaus.org"
-        //mavenRepo "http://repository.codehaus.org"
-        //mavenRepo "http://download.java.net/maven/2/"
-        //mavenRepo "http://repository.jboss.com/maven2/"
     }
     dependencies {
         // specify dependencies here under either 'build', 'compile', 'runtime', 'test' or 'provided' scopes eg.
 
         build "org.apache.maven:maven-ant-tasks:2.1.0",
               "org.codehaus.groovy.modules.http-builder:http-builder:0.5.0", {
-            excludes "commons-logging", "xml-apis"
+            excludes "commons-logging", "xml-apis", "groovy"
         }
         compile "org.tmatesoft.svnkit:svnkit:1.3.3", {
             excludes "jna", "trilead-ssh2", "sqljet"
@@ -35,6 +27,4 @@ grails.project.dependency.resolution = {
             export = false
         }
     }
-}
-
 }
