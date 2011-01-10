752a0da6feb281cbd9db3731ec3bed008049eefe
Peter Ledbrook <peter@cacoethes.co.uk>
2011-01-10 14:11:13 +0000

    Bumped plugin version number to 0.7.5.

--- MavenPublisherGrailsPlugin.groovy
+++ MavenPublisherGrailsPlugin.groovy
@@ -1,5 +1,5 @@
 class MavenPublisherGrailsPlugin {
-    def version = "0.7.4"
+    def version = "0.7.5"
     // the version or versions of Grails the plugin is designed for
     def grailsVersion = "1.2 > *"
     // resources that are excluded from plugin packaging
