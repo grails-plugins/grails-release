4e31379f4b66f586975fea4fc893d755a5d82861
Peter Ledbrook <peter@cacoethes.co.uk>
2011-04-29 19:59:44 +0100

    Updated plugin version, added licence header to plugin descriptor, and updated the issue management URL.

--- ReleaseGrailsPlugin.groovy
+++ ReleaseGrailsPlugin.groovy
@@ -1,5 +1,21 @@
+/*
+ * Copyright 2011 the original author or authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 class ReleaseGrailsPlugin {
-    def version = "1.0-SNAPSHOT"
+    def version = "1.0.0.BUILD-SNAPSHOT"
     def grailsVersion = "1.1 > *"
     def dependsOn = [svn: "1.0 > *"]
 
@@ -17,7 +33,7 @@ A plugin that allows you to publish Grails plugins, either to a public or privat
     def license = "APACHE"
     def organization = [ name: "SpringSource", url: "http://www.springsource.org/" ]
     def developers = [ [ name: "Peter Ledbrook", email: "pledbrook@vmware.com" ] ]
-    def issueManagement = [ system: "JIRA", url: "http://jira.codehaus.org/browse/GRAILSPLUGINS" ]
+    def issueManagement = [ system: "JIRA", url: "http://jira.grails.org/browse/GPMAVENPUBLISHER" ]
     def scm = [ url: "https://github.com/grails-plugins/grails-maven-publisher-plugin" ]
 
     def documentation = "http://github.com/grails-plugins/grails-maven-publisher-plugin"
