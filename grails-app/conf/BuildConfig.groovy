6e16229b0c89947de90af197d5efe2a7bd9a1f98
Luke Daley <ld@ldaley.com>
2012-03-16 15:08:05 +0000

    Correct rest-client plugin dependency to compile scope.
    Needed by GrailsCentralDeployer.

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -17,7 +17,7 @@ grails.project.dependency.resolution = {
     }
 
     plugins {
-        runtime ":rest-client-builder:1.0.2"
+        compile ":rest-client-builder:1.0.2"
     }
 
     dependencies {
