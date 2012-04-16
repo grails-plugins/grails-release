5407f6e3b3a53d81124b337cce815f4aeb6aba99
Burt Beckwith <beckwithb@vmware.com>
2012-04-16 19:30:08 -0400

    don't export the rest-client-builder plugin

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -1,6 +1,6 @@
 grails.project.work.dir = 'target'
-
 grails.project.docs.output.dir = "docs"
+grails.project.source.level = 1.6
 
 grails.project.dependency.resolution = {
 
@@ -17,7 +17,9 @@ grails.project.dependency.resolution = {
     }
 
     plugins {
-        compile ":rest-client-builder:1.0.2"
+        compile(":rest-client-builder:1.0.2") {
+            export = false
+        }
     }
 
     dependencies {
