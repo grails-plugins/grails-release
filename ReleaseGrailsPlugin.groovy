0bf7bb2941adfcb7c9b7327982dc5c3d0dda7859
Burt Beckwith <beckwithb@vmware.com>
2012-05-09 17:16:08 -0400

    2.0.1.BUILD-SNAPSHOT release

--- ReleaseGrailsPlugin.groovy
+++ ReleaseGrailsPlugin.groovy
@@ -1,5 +1,5 @@
 /*
- * Copyright 2011 the original author or authors
+ * Copyright 2011-2012 the original author or authors
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -15,8 +15,8 @@
  */
 
 class ReleaseGrailsPlugin {
-    def version = "2.0.0"
-    def grailsVersion = "1.2 > *"
+    def version = "2.0.1.BUILD-SNAPSHOT"
+    def grailsVersion = "2.0 > *"
     def author = "Graeme Rocher"
     def authorEmail = "grocher@vmware.com"
     def title = "Release Plugin"
